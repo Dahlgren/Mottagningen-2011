c934e9e3610fe545474be7f14b3785fda6f9ff04
Björn Dahlgren <bjorn@dahlgren.at>
2011-06-04 18:21:53 +0200

    Customized views in ActiveAdmin
    Signed-off-by: Björn Dahlgren <bjorn@dahlgren.at>

--- mottagning_app/app/admin/muxes.rb
+++ mottagning_app/app/admin/muxes.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Mux do
+  index do
+      column :id
+      column :name
+      column :title
+      column "Facebook ID", :facebookid
+      column :rank
+      default_actions
+    end
   
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :title
+      f.input :facebookid, :label => "Facebook ID"
+      f.input :rank
+    end
+    f.buttons
+  end
+  
+  filter :name
+  filter :facebookid
 end
