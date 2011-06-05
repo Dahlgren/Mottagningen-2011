1f0947a9d8baaca614f676e54b16a2bb505cda33
Albert Hafvenström <albhaf@gmail.com>
2011-06-05 16:18:30 +0200

    Shorter import
    Signed-off-by: Albert Hafvenström <albhaf@gmail.com>

--- mottagningen_score/urls.py
+++ mottagningen_score/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^score/', include('mottagningen_django.score.urls'))
+    url(r'^score/', include('score.urls'))
 )
