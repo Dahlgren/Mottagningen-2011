b814f3fd4a6bbfd90c20c319591ab8cce38d352d
Björn Dahlgren <bjorn@dahlgren.at>
2011-06-09 02:04:11 +0200

    Fixed minor mistake while querying mux
    Signed-off-by: Björn Dahlgren <bjorn@dahlgren.at>

--- mottagning_app/app/controllers/people_controller.rb
+++ mottagning_app/app/controllers/people_controller.rb
@@ -2,9 +2,9 @@ class PeopleController < ApplicationController
   def show
     @fadders = Fadder.order('name')
     @muxes = {
-      "3" => Mux.order('name').where("rank == 3"),
-      "2" => Mux.order('name').where("rank == 2"),
-      "1" => Mux.order('name').where("rank == 1")
+      "3" => Mux.order('name').where("rank = 3"),
+      "2" => Mux.order('name').where("rank = 2"),
+      "1" => Mux.order('name').where("rank = 1")
     }
   end
 end
