54b746c7b9a379659317d6f431bb8faec9dffad8
Albert Hafvenström <albhaf@gmail.com>
2011-08-16 13:01:43 +0200

    Order filtered days by date to account for days with higher id but lower date
    Signed-off-by: Albert Hafvenström <albhaf@gmail.com>

--- mottagningen_score/score/api/handlers.py
+++ mottagningen_score/score/api/handlers.py
@@ -17,7 +17,7 @@ class ScoreHandler(BaseHandler):
         score = {}
         if (date == "0"):
             groups = Group.objects.all()
-            days = Day.objects.filter(date__lte=currentDate())
+            days = Day.objects.filter(date__lte=currentDate()).order_by('date')
             for g in groups:
                 groupInfo = []
                 for d in days:
