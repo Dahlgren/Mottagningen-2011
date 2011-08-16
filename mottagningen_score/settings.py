bff18fd79fe8feea85467b2fa55d589dc23fff6d
Björn Dahlgren <bjorn@dahlgren.at>
2011-08-16 19:03:15 +0200

    Cleanup in settings

--- mottagningen_score/settings.py
+++ mottagningen_score/settings.py
@@ -1,6 +1,8 @@
 # Django settings for mottagningen_django project.
 
-import os.path
+import os
+
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -47,18 +49,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/alberthafvenstrom/Code/mottagningen_django/media'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,7 +73,6 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -106,8 +107,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mottagningen_score.urls'
 
 TEMPLATE_DIRS = (
-    '/Users/alberthafvenstrom/Code/mottagningen_score/templates/',
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')
+    os.path.join(PROJECT_ROOT, 'templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
