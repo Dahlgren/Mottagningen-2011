817fe523137587e8cd680415c93b24932f98ccfe
Albert Hafvenström <albhaf@gmail.com>
2011-08-17 14:09:01 +0200

    Sort days in frontend
    Signed-off-by: Albert Hafvenström <albhaf@gmail.com>

--- mottagningen_score/score/static/score/js/graph.js
+++ mottagningen_score/score/static/score/js/graph.js
@@ -17,6 +17,7 @@ function requestMain() {
         $.each(days, function(index, name) {
             mainOptions.xAxis.categories.push(name);
         });
+        mainOptions.xAxis.categories.sort();
         
         $.each(groups, function(id, group) {
             $.each(group, function(name, scores) {
